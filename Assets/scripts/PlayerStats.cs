b34036adb5bf10b88ae4bd56f1dfc14e654c21bd
NathanLazo <nathanlazo@outlook.com>
2022-11-17 09:59:11 -0600

    v0.05  [UPDATE] enemy, player script (baby jorsh)

---
+++ Assets/scripts/PlayerStats.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    private float life = 100;
+    private float maxLife = 100;
+    private float stamina = 50;
+    private float maxStamina = 50;
+    private float damage = 10;
+
+    
+
+}
--- Assets/scripts/ThirdPersonController.cs
+++ Assets/scripts/ThirdPersonController.cs
@@ -7,30 +7,71 @@ public class ThirdPersonController : MonoBehaviour
 
     public CharacterController controller;
 
+    public Vector3 moveDir;
+
     public float speed = 6f;
+    public float jumpHeight = 3f;
 
     public float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
 
     public Transform cam;
 
-    // Update is called once per frame
+    public bool isGrounded = true;
+
+    public float fallVelocity = 0f;
+
+    public float gravity = 9.81f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
     void Update()
     {
+        SetMovement();
+        SetGravity();
+    }
+
+    void SetGravity()
+    {
+        if (isGrounded)
+        {
+            fallVelocity = -gravity * Time.deltaTime;
+            moveDir.y = fallVelocity;
+        }
+        else
+        {
+            fallVelocity -= gravity * Time.deltaTime;
+            moveDir.y = fallVelocity;
+        }
+    }
+
+    void SetMovement()
+    {
         float x = Input.GetAxisRaw("Horizontal");
-        float y = Input.GetAxisRaw("Vertical");
-        Vector3 move = new Vector3(x, 0f, y).normalized;
+        float z = Input.GetAxisRaw("Vertical");
+        Vector3 move = new Vector3(x, 0f, z);
 
         if (move.magnitude >= 0.1f)
         {
             float targetAngle = Mathf.Atan2(move.x, move.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
             float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
-           
+
             transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
 
-            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
             controller.Move(moveDir.normalized * speed * Time.deltaTime);
+
+            if (Input.GetKeyDown("space"))
+            {
+                Debug.Log("Dash animation");
+                //move controller forward
+                controller.Move(moveDir.normalized * speed * Time.deltaTime * 16);
+            }
         }
+
     }
 }
